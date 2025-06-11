44ba46e30ca932f5059716348c57dfa51b859b75
Joey Fang <josudoey@gmail.com>
2025-06-11 16:11:34 +0800

    feat: add react-basic

---
+++ apps/react-basic/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+        {/* see https://nextjs.org/docs/app/getting-started/fonts */}
+        <div className='font-[family-name:var(--font-geist-mono)]'>
+          Hello World
+        </div>
+      </main>
+      <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'></footer>
+    </div>
+  )
+}
