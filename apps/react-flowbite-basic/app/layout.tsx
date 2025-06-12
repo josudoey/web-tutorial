6fb7c6075fc41ce26e46be8757d7c5ef6c85fc60
Joey Fang <josudoey@gmail.com>
2025-06-12 21:47:11 +0800

    feat: add react-flowbite-basic basic

---
+++ apps/react-flowbite-basic/app/layout.tsx
@@ -0,0 +1,25 @@
+import { ThemeModeScript } from "flowbite-react";
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <ThemeModeScript />
+      </head>
+      <body>
+        <Navbar />
+        <div className="flex items-start">
+          <Sidebar />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
